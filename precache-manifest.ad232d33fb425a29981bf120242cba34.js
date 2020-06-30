8ca726a655688d3890e424c68713cddc84c80f61
MightyMoin <lcs2019006@iiitl.ac.in>
2020-06-30 20:28:50 +0530

    Updates

---
+++ precache-manifest.ad232d33fb425a29981bf120242cba34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fe0df975162ad63913a0a08857e0eb",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "fc1b4a2caffbc4a50f59",
+    "url": "/contactManager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b55b38d0114c1ba3f3ed",
+    "url": "/contactManager/static/css/main.aafcf9b0.chunk.css"
+  },
+  {
+    "revision": "fc1b4a2caffbc4a50f59",
+    "url": "/contactManager/static/js/2.64e4c5f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactManager/static/js/2.64e4c5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b55b38d0114c1ba3f3ed",
+    "url": "/contactManager/static/js/main.0520f383.chunk.js"
+  },
+  {
+    "revision": "48652ea2756f83312194",
+    "url": "/contactManager/static/js/runtime-main.41e32883.js"
+  }
+]);
